8fdf3d29a2982e7b054c615dee553e10bd2e6404
sdutwwb <sdutwwb@163.com>
2017-10-19 22:50:57 +0800

    注册页面修改

--- index/controller/User.php
+++ index/controller/User.php
@@ -15,23 +15,48 @@ class User extends Controller
 	{
 		return $this->fetch();
 	}
-	//检查验证码
-	public function checkreg()
+	//检查邮箱
+	public function checkemail()
 	{
 		
-		$arr = $this->request->param('code');
+		$email = $this->request->param('email');
+		$validate = new Validate([
+			'email' => 'email'
+			]);
+		$data = [
+			'email' => $email,
+		];
+		if (!$validate->check($data)) {
+			echo json_encode(['code'=>1 , 'message' => '亲，你输入的邮箱格式不正确哦']);
+		} else {
+			$data = $this->user->where('user_name', $email)->find();
+			if ($data) {
+				echo json_encode(['code'=>2 , 'message' => '亲，您输入的邮箱已被注册了哟']);
+			} else {
+				echo json_encode(['code'=>0 , 'message' => '亲，您输入的邮箱可用了哟']);
+			}
+		}
+	}
+	//验证码验证
+	public function checkcode()
+	{
+		$code = $this->request->param('code');
 		$validate = new Validate([
 			'captcha|验证码'=>'require|captcha'
 		]);
 		$data = [
-			'captcha'=>input('code'),
+			'captcha'=>$code,
 		];
 		if (!$validate->check($data)) {
-			dump(input('code'));
-			dump($validate->getError());
+			echo json_encode(['code'=>3 , 'message' => '亲，验证码错了哟']);
 		} else {
-			$this->success('注册成功', 'index/index');
+			echo json_encode(['code'=>0 , 'message' => '快点点击注册吧']);
 		}
+	}
+	//插入数据
+	public function checkreg()
+	{
+
 	}
 	public function regphone()
 	{
