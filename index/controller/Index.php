764028c62730a9a21257f52d1bca86d93c4dd3ea
sdutwwb <sdutwwb@163.com>
2017-10-20 22:14:23 +0800

    前段页面的模板引擎搭建

--- index/controller/Index.php
+++ index/controller/Index.php
@@ -1,7 +1,7 @@
 <?php
 namespace app\index\controller;
 
-use  \think\Controller;
+use \think\Controller;
 use app\index\model\User as UserModel;
 use \think\Validate;
 
@@ -9,7 +9,34 @@ class Index extends Controller
 {
 	public function index()
 	{
-		// $this->success('haha','index/index');
+		return $this->fetch();
+	}
+	public function blog()
+	{
+		return $this->fetch();
+	}
+	public function blog_post()
+	{
+		return $this->fetch();
+	}
+	public function contact()
+	{
+		return $this->fetch();
+	}
+	public function detail()
+	{
+		return $this->fetch();
+	}
+	public function listing()
+	{
+		return $this->fetch();
+	}
+	public function news()
+	{
+		return $this->fetch();
+	}
+	public function about()
+	{
 		return $this->fetch();
 	}
 }
