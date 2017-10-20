429e1def64db94c65e41854118c80ed8894efbce
sdutwwb <sdutwwb@163.com>
2017-10-20 14:17:36 +0800

    手机激活码第一次验证成功

--- index/controller/User.php
+++ index/controller/User.php
@@ -37,6 +37,18 @@ class User extends Controller
 			}
 		}
 	}
+	//手机号验证
+	public function checkphone()
+	{
+		$phone = $this->request->param('phone');
+		$data = $this->user->where('user_name', $phone)->find();
+			if ($data) {
+				echo json_encode(['code'=>2 , 'message' => '亲,您输入的手机号已被注册了哟']);
+			} else {
+				echo json_encode(['code'=>0 , 'message' => '亲,您输入的手机号可用了哟']);
+			}
+
+	}
 	//验证码验证
 	public function checkcode()
 	{
@@ -53,6 +65,17 @@ class User extends Controller
 			echo json_encode(['code'=>0 , 'message' => '快点点击注册吧']);
 		}
 	}
+	//手机激活码验证
+	public function checkpricode()
+	{
+		$pricode = $this->request->param('pricode');
+		dump($pricode);
+		if (!$) {
+			echo json_encode(['code'=>3 , 'message' => '亲，验证码错了哟']);
+		} else {
+			echo json_encode(['code'=>0 , 'message' => '快点点击注册吧']);
+		}
+	}
 	//插入数据
 	public function checkreg()
 	{
@@ -62,5 +85,67 @@ class User extends Controller
 	public function regphone()
 	{
 		return $this->fetch();
+	}
+	//手机激活码
+	public function ucpaas()
+	{
+		if (!isset($_POST['phone'])) {
+			exit;
+		}
+		$phone = $_POST['phone'];
+		define('BASE_URL', 'https://api.ucpaas.com/');
+		define('SOFT_VERSION','2014-06-30');
+
+		$accountSid = '2e24761cd9beb800c53e5bd5a2ffa7ca';
+		$authorToken = 'be91ff76da2879a42a483802412343b6';
+		//设置默认时区
+		date_default_timezone_set('PRC');
+		$timestamp = date('YmdHis');
+
+		$sigParameter = strtoupper(md5($accountSid.$authorToken.$timestamp));
+
+		$url = BASE_URL . SOFT_VERSION . '/Accounts/' . $accountSid . '/Messages/templateSMS?sig=' . $sigParameter;
+
+		$authorization = base64_encode($accountSid.':'.$timestamp);
+		//拼接header
+		$header = [
+			'Accept:application/json',
+			'Content-Type:application/json;charset=utf-8',
+			'Authorization:'.$authorization
+		];
+
+	$nums = '1234567890';
+	$yzm = substr(str_shuffle($nums),0,4);
+		$data['templateSMS'] = [
+								'appId'=>'5e6cbb9fc7244006beac494b849111e5',
+								'templateId'=>'153890',
+								'to'=>$phone,
+								'param'=>$yzm
+							];
+
+		$body = json_encode($data);
+
+		//发送请求
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+		curl_setopt($ch,CURLOPT_POST,1);
+		curl_setopt($ch,CURLOPT_POSTFIELDS,$body);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+		$result = curl_exec($ch);
+		curl_close($ch);
+		//var_dump($result);
+		$result = json_decode($result, true);
+		$respCode = $result['resp']['respCode'];
+
+		if ($respCode == '000000') {
+			$send = 'ok';
+			//将验证码保存在session中
+			Session::set('yzm',$yzm);
+		} else {
+			$send = 'err';
+		}
+		echo json_encode(['send' => $send]);
 	}
 }
