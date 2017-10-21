acef9b7370146d42360502e4f397c4ba5524e378
sdutwwb <sdutwwb@163.com>
2017-10-21 22:40:42 +0800

    登录页面 首页界面优化

--- index/controller/Index.php
+++ index/controller/Index.php
@@ -4,6 +4,7 @@ namespace app\index\controller;
 use \think\Controller;
 use app\index\model\User as UserModel;
 use \think\Validate;
+use \think\Session;
 
 class Index extends Controller
 {
@@ -38,5 +39,41 @@ class Index extends Controller
 	public function about()
 	{
 		return $this->fetch();
+	}
+	public function mmzh()
+	{
+		return $this->fetch();
+	}
+	public function log()
+	{
+		return $this->fetch();
+	}
+	//验证码验证
+	public function checkcode()
+	{
+		$code = $this->request->param('code');
+		$validate = new Validate([
+			'captcha|验证码'=>'require|captcha'
+		]);
+		$data = [
+			'captcha'=>$code,
+		];
+		if (!$validate->check($data)) {
+			echo json_encode(['code'=>1]);
+		} else {
+			echo json_encode(['code'=>0]);
+		}
+	}
+	//验证用户
+	public function checkuser()
+	{
+		$data = $this->request->param();
+		dump($data);
+		echo 1;
+	}
+	//
+	public function checkx()
+	{
+		$this->success('dsad');
 	}
 }
