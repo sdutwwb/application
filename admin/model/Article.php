bd5e6ae536eb9c7d04913aed8b69c124b5dc2930
wanghuashun <957004161@qq.com>
2017-10-21 21:54:48 +0800

    后台文章处理

--- admin/model/Article.php
+++ admin/model/Article.php
@@ -4,5 +4,36 @@ namespace app\admin\model;
 use think\Model;
 class Article extends Model
 {
-	
+	//文章审核状态转换
+	public function getStatusAttr($value) {
+		$status = [1=>'未审核',0=>'通过',2=>'通过并推荐'];
+		return $status[$value];
+	}
+
+	//对对关系
+	public function touser()//在user控制器中反查询用户信息
+	{
+		return $this->belongsTo('user', 'uid');
+	}
+	public function getuser()//一对一去查询用的信息
+	{
+		return $this->hasOne('user','uid');
+	}
+
+
+	//封装的方法
+	public function getallArticle()//得到所有未处理的文章
+	{
+		return $this->where('status', '1')->select();
+	}
+	public function getsimpleArticle($aid)
+	{
+		return $this->where('aid',"$aid")->find();
+	}
+	public function solveimage($allimage)
+	{
+		$str1 = trim($allimage,'&');
+		$arr = explode('&', $str1);
+		return $arr;
+	}
 }
--- admin/model/User.php
+++ admin/model/User.php
@@ -4,5 +4,24 @@ namespace app\admin\model;
 use think\Model;
 class User extends Model
 {
-	
+	public function onetomanyarticle()//1对多关联article表用与查询用户的发帖信息
+	{
+		return $this->hasMany('article','uid');
+	}
+	public function getMemberAttr($value)
+	 {
+		$member = [1=>'会员',0=>'非会员'];
+		return $member[$value];
+	}
+	public function getScoreAttr($value)
+	 {
+		$va = floor($value/50);
+		$score = ['0'=>"$va"];
+		return $score['0'];
+	}
+	public function getSexAttr($value)
+	{
+		$sex = [0 => '保密', 2=>'女',1=>'男'];
+		return $sex[$value];
+	}
 }
