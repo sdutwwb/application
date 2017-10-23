94c13524183ba6d2d0db8702d7c25a5834ccddbc
wanghuashun <957004161@qq.com>
2017-10-23 08:41:22 +0800

    后台第二页

--- admin/model/Article.php
+++ admin/model/Article.php
@@ -6,7 +6,7 @@ class Article extends Model
 {
 	//文章审核状态转换
 	public function getStatusAttr($value) {
-		$status = [1=>'未审核',0=>'通过',2=>'通过并推荐'];
+		$status = [1=>'未审核',0=>'通过',3=>'通过并推荐',2=>'未通过'];
 		return $status[$value];
 	}
 
@@ -26,14 +26,20 @@ class Article extends Model
 	{
 		return $this->where('status', '1')->select();
 	}
-	public function getsimpleArticle($aid)
+	public function getsimpleArticle($aid)//得到单个未处理的文章
 	{
 		return $this->where('aid',"$aid")->find();
 	}
-	public function solveimage($allimage)
+	public function solveimage($allimage)//处理多个照片
 	{
 		$str1 = trim($allimage,'&');
 		$arr = explode('&', $str1);
 		return $arr;
+	}
+	public function setArticleStatus($data)
+	{
+		$status = $data['status'];
+		$aid = $data['aid'];
+		return $this->save(['status'=>"$status"],['aid'=>"$aid"]);
 	}
 }
